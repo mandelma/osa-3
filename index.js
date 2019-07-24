801d1d30180725a65d92aafb1184750bb0eebcb9
Marko <majaana@live.com>
2019-07-24 12:03:29 +0300

    backend step 6

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
 let persons = [
 
     {
@@ -35,17 +39,64 @@ app.get('/api', (req, res) => {
 	res.send('<h1>Hei maailma</h1>')
 })
 
-app.get('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id)
-	const person = persons.find(p => p.id === id)
-	console.log(id)
-	console.log(person)
-	if(person){
-		res.json(person)
-	}else{
-		res.status(404).end()
-	}
+app.get('/api/persons', (req, res) => {
+	//const id = Number(req.params.id)
+	//const person = persons.find(p => p.id === id)
+	//console.log(id)
+	//console.log(person)
+	//if(person){
+	//	res.json(person)
+	//}else{
+	//	res.status(404).end()
+	res.json(persons)
+	
 })
+
+const generateId = () => {
+	const min = persons.length + 1
+	const max = 30
+	const maxId = persons.length > 0
+		? Math.random() * (max - min) + min
+		: 0
+	return Math.ceil(maxId)
+}
+
+
+app.post('/api/persons', (req, res) => {
+	const body = req.body
+	
+	if(!body.name || !body.number){
+		return res.status(400).json({
+			error: 'name or number are missing'
+		})
+	}
+	
+	if(persons.find(n => n.name === body.name)){
+		return res.status(400).json({
+			error: 'name already exists'
+		})
+	}
+	
+	
+	const person = {
+		id: generateId(),
+		name: body.name,
+		number: body.number
+	}
+	persons = persons.concat(person)
+	
+	res.json(person)
+})
+
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	person = persons.filter(name => name.id === id)
+	console.log(id)
+	response.status(204).end()
+})
+
+
 
 
 const PORT = 3001
