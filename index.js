960ed2aeded50f1163de84aeb26fcb3d2f00f094
Marko <majaana@live.com>
2019-08-11 21:06:06 +0300

    puhelinluettelo ja tietokanta, step 2

--- index.js
+++ index.js
@@ -34,13 +34,29 @@ let persooons = [
 
 app.use(express.static('build'))
 
-const info = () => {
-	const count = persons.length
-	const date = new Date().toLocaleString()
-return `Phonebook has info for ${count} people` + "<br><br>" + ` ${date}`
-}
-	
+//const info = () => {
+//	const count = persons.length
+//	const date = new Date().toLocaleString()
+//return `Phonebook has info for ${count} people` + "<br><br>" + ` ${date}`
+//}
 
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
+})
 
 app.get('/info', (req, res) => {
 	res.send(info())
@@ -56,70 +72,48 @@ app.get('/', (req, res) => {
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons.map(person => person.toJSON()))
+	console.log("db pikkus on: " + persons.length)
   });
 });
 
-//app.get('/api/persons', (req, res) => {
-	//const id = Number(req.params.id)
-	//const person = persons.find(p => p.id === id)
-	//console.log(id)
-	//console.log(person)
-	//if(person){
-	//	res.json(person)
-	//}else{
-	//	res.status(404).end()
-	
-	//res.json(persons)
-	
-//	res.send(persons)
-	
-//})
-
-const generateId = () => {
-	const min = persons.length + 1
-	const max = 30
-	const maxId = persons.length > 0
-		? Math.random() * (max - min) + min
-		: 0
-	return Math.ceil(maxId)
-}
-
-
-app.post('/api/persons', (req, res) => {
-	const body = req.body
-	
-	if(!body.name || !body.number){
-		return res.status(400).json({
-			error: 'name or number are missing'
-		})
-	}
-	
-	if(persons.find(n => n.name === body.name)){
-		return res.status(400).json({
-			error: 'name already exists'
-		})
-	}
-	
-	
-	const person = {
-		id: generateId(),
-		name: body.name,
-		number: body.number
-	}
-	persons = persons.concat(person)
-	console.log(req.body)
-	res.json(person)
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(note => {
+    response.json(note.toJSON())
+  })
 })
 
 
 
+//const generateId = () => {
+//	const min = persons.length + 1
+//	const max = 30
+//	const maxId = persons.length > 0
+//		? Math.random() * (max - min) + min
+//		: 0
+//	return Math.ceil(maxId)
+//}
 
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	person = persons.filter(name => name.id !== id)
-	persons = person
-	response.status(204).end()
+
+
+
+	
+
+
+app.delete('/api/persons/:id', (req, res) => {
+	Person.deleteOne({ _id: req.params.id}, (err, result) => {
+		if(err) {
+			res.status(404).send(err)
+		}
+		
+		res.status(200).json({ message: "Task successfully deleted" })
+		
+		
+
+	})
+	
 })
+
+
 
 
 
